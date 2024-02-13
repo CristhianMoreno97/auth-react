c0f8d3a8dd6c291f7105f734874bc3b26f850a9b
CristhianMoreno97 <cristian_21_97@hotmail.com>
2024-02-13 08:53:32 -0500

    feat: Add react-router-dom for routing
    - Added react-router-dom to dependencies
    - Updated main.tsx to use react-router-dom for routing
    - Created Login, SignUp, and Dashboard route components

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,28 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
 import './index.css'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import SignUp from './routes/SignUp.tsx'
+import Dashboard from './routes/Dashboard.tsx'
+import Login from './routes/Login.tsx'
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Login />
+  },
+  {
+    path: "/signup",
+    element: <SignUp />,
+  },
+  {
+    path: "/dashboard",
+    element: <Dashboard />,
+  }
+])
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
