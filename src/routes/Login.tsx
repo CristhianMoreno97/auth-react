41100c5696ce482bae39e9c08db45c819d55da63
CristhianMoreno97 <cristian_21_97@hotmail.com>
2024-02-13 14:31:19 -0500

    feat(routes): Update Login and SignUp components
    - Updated Login.tsx to include form elements for username and password
    - Integrated DefaultLayout into Login.tsx for consistent UI
    - Updated SignUp.tsx to include form elements for name, username, and password
    - Integrated DefaultLayout into SignUp.tsx for consistent UI

--- src/routes/Login.tsx
+++ src/routes/Login.tsx
@@ -1,5 +1,38 @@
-export default function login() {
+import { useState } from "react";
+import DefaultLayout from "../layouts/DefaultLayout";
+import { useAuth } from "../features/authentication/AuthProvider";
+import { Navigate } from "react-router-dom";
+
+export default function Login() {
+
+    const auth = useAuth()
+    if (auth.isAuthenticated) {
+        return <Navigate to="/dashboard" />
+    }
+
+    const [username, setUsername] = useState("")
+    const [password, setPassword] = useState("")
     return (
-        <h1>Login</h1>
+        <DefaultLayout>
+            <form action="">
+                <h1>Login</h1>
+
+                <label>Username</label>
+                <input 
+                    type="text" 
+                    value={username}
+                    onChange={(e) => setUsername(e.target.value)}
+                />
+
+                <label>Password</label>
+                <input 
+                    type="password" 
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                />
+
+                <button>Login</button>
+            </form>
+        </DefaultLayout>
     )
 }
--- src/routes/SignUp.tsx
+++ src/routes/SignUp.tsx
@@ -1,5 +1,46 @@
+import { useState } from "react";
+import DefaultLayout from "../layouts/DefaultLayout";
+import { useAuth } from "../features/authentication/AuthProvider";
+import { Navigate } from "react-router-dom";
+
 export default function SignUp() {
+
+    const auth = useAuth()
+    if (auth.isAuthenticated) {
+        return <Navigate to="/dashboard" />
+    }
+
+    const [name, setName] = useState("")
+    const [username, setUsername] = useState("")
+    const [password, setPassword] = useState("")
     return (
-        <h1>Sign up</h1>
+        <DefaultLayout>
+            <form action="">
+                <h1>Sign up</h1>
+
+                <label>Name</label>
+                <input 
+                    type="text"
+                    value={name}
+                    onChange={(e) => setName(e.target.value)}
+                />
+
+                <label>Username</label>
+                <input 
+                    type="text" 
+                    value={username}
+                    onChange={(e) => setUsername(e.target.value)}
+                />
+
+                <label>Password</label>
+                <input 
+                    type="password" 
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                />
+
+                <button>Create Account</button>
+            </form>
+        </DefaultLayout>
     )
 }
