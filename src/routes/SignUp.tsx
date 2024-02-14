29df85ca44e0cc261a4586da0ebc853eb52c215d
CristhianMoreno97 <cristian_21_97@hotmail.com>
2024-02-14 14:33:40 -0500

    feat(signup): Implement sign-up functionality and error handling
    - Updated Signup.tsx to handle form submission for sign-up
    - Integrated useNavigate hook from react-router-dom for navigation
    - Implemented error handling for failed sign-up attempts
    - Refactored form submission to use asynchronous fetch API

--- src/routes/SignUp.tsx
+++ src/routes/SignUp.tsx
@@ -1,11 +1,14 @@
 import { useState } from "react";
 import DefaultLayout from "../layouts/DefaultLayout";
 import { useAuth } from "../features/authentication/AuthProvider";
-import { Navigate } from "react-router-dom";
+import { Navigate, useNavigate } from "react-router-dom";
+import { AuthResponseError } from "../features/authentication/types";
 
 export default function SignUp() {
 
     const auth = useAuth()
+    const goTo = useNavigate()
+
     if (auth.isAuthenticated) {
         return <Navigate to="/dashboard" />
     }
@@ -13,10 +16,45 @@ export default function SignUp() {
     const [name, setName] = useState("")
     const [username, setUsername] = useState("")
     const [password, setPassword] = useState("")
+    const [errorResponse, setErrorResponse] = useState("")
+
+    const apiUrl = import.meta.env.VITE_APP_API_URL
+
+    async function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
+        e.preventDefault()
+        try {
+            const response = await fetch(`${apiUrl}/signup`, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({
+                    name,
+                    username,
+                    password
+                })
+            })
+
+            if (response.ok) {
+                console.log('User created successfully')
+                setErrorResponse("")
+                goTo("/")
+            } else {
+                console.log('Something went wrong')
+                const json = (await response.json()) as AuthResponseError
+                setErrorResponse(json.body.error)
+                return
+            }
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
     return (
         <DefaultLayout>
-            <form action="">
+            <form onSubmit={handleSubmit}>
                 <h1>Sign up</h1>
+                {errorResponse && <p>{errorResponse}</p>}
 
                 <label>Name</label>
                 <input 
