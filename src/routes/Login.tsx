77e2370214be9c6a721b1547391b04818b177feb
CristhianMoreno97 <cristian_21_97@hotmail.com>
2024-02-14 14:31:28 -0500

    feat(login): Implement login functionality and error handling
    - Updated Login.tsx to handle form submission for login
    - Integrated useNavigate hook from react-router-dom for navigation
    - Implemented error handling for failed login attempts

--- src/routes/Login.tsx
+++ src/routes/Login.tsx
@@ -1,21 +1,55 @@
 import { useState } from "react";
 import DefaultLayout from "../layouts/DefaultLayout";
 import { useAuth } from "../features/authentication/AuthProvider";
-import { Navigate } from "react-router-dom";
+import { Navigate, useNavigate } from "react-router-dom";
+import { AuthResponseError } from "../features/authentication/types";
 
 export default function Login() {
 
     const auth = useAuth()
+    const goTo = useNavigate()
+
     if (auth.isAuthenticated) {
         return <Navigate to="/dashboard" />
     }
 
     const [username, setUsername] = useState("")
     const [password, setPassword] = useState("")
+    const [errorResponse, setErrorResponse] = useState("")
+
+    const apiUrl = import.meta.env.VITE_APP_API_URL
+
+    async function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
+        e.preventDefault()
+        try {
+            const response = await fetch(`${apiUrl}/login`, {
+                method: 'POST',
+                headers: {'Content-Type': 'application/json'},
+                body: JSON.stringify({
+                    username,
+                    password
+                })
+            })
+
+            if (response.ok) {
+                console.log('User Logged in')
+                setErrorResponse("")
+                goTo("/")
+            } else {
+                console.log('Something went wrong')
+                const json = (await response.json()) as AuthResponseError
+                setErrorResponse(json.body.error)
+            }
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
     return (
         <DefaultLayout>
-            <form action="">
+            <form onSubmit={handleSubmit}>
                 <h1>Login</h1>
+                {errorResponse && <p>{errorResponse}</p>}
 
                 <label>Username</label>
                 <input 
